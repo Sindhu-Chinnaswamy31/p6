efa96f98625700fddac637af018c1c58825fabda
Your Name <sindhucswamy@gmail.com>
2020-08-26 19:09:21 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -7,10 +7,15 @@ from app import utitlities
 def home(request):
     if request.method=="POST":
         form=forms.SampleForm(request.POST,request.FILES)
-        if form.is_valid():
+        if form.is_valid()==False:
+            return render(request,"app/sample1.html",{'form':form})
+
+        else:
             data=form.cleaned_data
             profile_pic=data['profile_pic']
             utitlities.store_image(profile_pic)
-            return HttpResponse(str(data))
+            print(form.cleaned_data)
+            
+    
     form=forms.SampleForm()
     return render(request,"app/sample1.html",{'form':form})
