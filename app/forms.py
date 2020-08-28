58da18e47c2893fc022446ea8a15ff84d0e1c8ce
Your Name <sindhucswamy@gmail.com>
2020-08-28 09:39:12 +0530

    third commit

--- app/forms.py
+++ app/forms.py
@@ -4,10 +4,15 @@ from django.core.exceptions import ValidationError
 import re
 
 def validate_name(name):
+    if len(name)>20:
+        raise ValidationError("Name is Not Valid")
+    return name
+"""
     m=re.match('[a-zA-Z]+',name)
     if m.group()!=name:
         raise ValidationError("Name is Not valid")
     return name
+"""
 
 class SampleForm(forms.Form):
     name=forms.CharField(max_length=200,required=True,label="Name :",
@@ -21,6 +26,7 @@ class SampleForm(forms.Form):
     validators=[validators.validate_ipv4_address])
     pwd=forms.CharField(max_length=200,required=True,label="Password :",widget=forms.PasswordInput(attrs={'placeholder':"Password"}))
     profile_pic=forms.ImageField(max_length=200,required=True,label="Profile Pic :")
+    botcacher=forms.CharField(max_length=10,required=False,widget=forms.HiddenInput)
 
     def clean(self,*args,**kwargs):
         cleaned_data=super().clean()#getting all the data that is filled in the form
@@ -29,7 +35,20 @@ class SampleForm(forms.Form):
         if email==cemail:
             return cleaned_data
         self.add_error('confirm_email',"Both the emails are not same")#this method will decide to which field we need to show the error
-        #self.add_error("fieldname",error message)
+        self.add_error('email',"Both the emails are not same")#self.add_error("fieldname",error message)
+
+    def clean_name(self):
+        name=self.cleaned_data.get("name") #is used to get the name that we have filled in the form
+        for i in name:
+            if not('a'<=i<='z' or 'A'<=i<='Z'):
+                raise ValidationError("Name must contain only alphabets")
+        return name
+
+    def clean_botcacher(self):
+        data=self.cleaned_data.get("botcacher")
+        if len(data)==0:
+            return data
+        self.add_error("name","invalid HUMAN")
         
 
     # def clean(self,*args,**kwargs):
